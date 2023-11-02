b55af23df5bb498b93e7c776c82add63934cbeed
Onurcan Kurt <onurcankurt96@gmail.com>
2023-11-02 19:15:46 +0300

    Challange 1 added

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    private static Vector3 thirdViewOffset = new Vector3(0, 5, -7);
+    private static Vector3 firstViewOffset = new Vector3(0, 2, 1);
+    private Vector3 offset = thirdViewOffset;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,14 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+        if(offset == thirdViewOffset && Input.GetKeyDown(KeyCode.V))
+        {
+            offset = firstViewOffset;
+            
+        } else if(offset == firstViewOffset && Input.GetKeyDown(KeyCode.V))
+        {
+            offset = thirdViewOffset;
+        }
         transform.position = player.transform.position + offset;
     }
 }
